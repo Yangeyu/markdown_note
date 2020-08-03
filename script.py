2e8398ec0c602a258b623e7b4ac14e391f8b39ae
Yang <840904451@qq.com>
2020-08-03 23:45:03 +0800

    commit

--- script.py
+++ script.py
@@ -5,8 +5,4 @@ os.system('d: && \
 	git add . && \
 	git commit -m "commit" && \
 	git push')
-# os.system('dir')
-# os.system('git add .')
-# os.system('git commit -m "commit"')
-# os.system('git push')
-input('end - ')
+# input('end - ')
