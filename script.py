34e22a3f65e19c63e869ba164583f5247bb58df9
Yang <840904451@qq.com>
2020-08-03 23:40:33 +0800

    commit

--- script.py
+++ script.py
@@ -1,5 +1,11 @@
 import os
-os.system('git add .')
-os.system('git commit -m "commit"')
-os.system('git push')
-# input('end - ')
+os.system('cd D:/Repositories/markdown_note && \
+ dir && \
+ git add . && \
+ git commit -m "commit" && \
+ git push')
+# os.system('dir')
+# os.system('git add .')
+# os.system('git commit -m "commit"')
+# os.system('git push')
+input('end - ')
